1524cd02c991700d2ff61ca52d033e36afab8ad8
kozuberda <tanya1frolova@gmail.com>
2016-02-03 09:49:20 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-;(function(){
+;(function($){
+	$(function(){
+		var body = $('body'),
+		    menuToggle = $('.ba-menu-toggle');
 
-})();
+	    menuToggle.click(function(event) {
+	    	event.preventDefault();
+	    	body.toggleClass('ba-menu-opened')
+	    });
+	});
+})(jQuery);
